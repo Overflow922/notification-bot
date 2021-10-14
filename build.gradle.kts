8d43836bd280c5035b62c7f3fef2386b7a3e536e
Overflow922 <genius922@yandex.ru>
2021-10-15 00:18:24 +0300

    init pipeline

---
+++ build.gradle.kts
@@ -0,0 +1,93 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat.*
+import org.gradle.api.tasks.testing.logging.TestLogEvent.*
+
+plugins {
+    // Apply the java-library plugin to add support for Java Library
+    `java-library`
+    id("com.github.spotbugs") version "4.6.0"
+    jacoco
+}
+
+repositories {
+    // Use jcenter for resolving your dependencies.
+    // You can declare any Maven/Ivy/file repository here.
+    jcenter()
+    mavenCentral()
+}
+
+dependencies {
+    // This dependency is exported to consumers, that is to say found on their compile classpath.
+    api("org.apache.commons:commons-math3:3.6.1")
+
+    api("org.apache.logging.log4j:log4j-api:2.14.0")
+    api("org.apache.logging.log4j:log4j-core:2.14.0")
+    api("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.12.1")
+    api("com.fasterxml.jackson.core:jackson-databind:2.12.1")
+
+    // This dependency is used internally, and not exposed to consumers on their own compile classpath.
+    implementation("com.google.guava:guava:31.0.1-jre")
+
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
+    testRuntimeOnly ("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+}
+
+buildscript {
+    repositories {
+        maven {
+            url = uri("https://plugins.gradle.org/m2/")
+        }
+    }
+    dependencies {
+        classpath("gradle.plugin.com.github.spotbugs.snom:spotbugs-gradle-plugin:4.6.0")
+    }
+}
+
+apply(plugin = "com.github.spotbugs")
+
+tasks.test {
+    useJUnitPlatform {}
+    testLogging {
+        events(FAILED, STANDARD_ERROR, SKIPPED)
+        exceptionFormat = FULL
+        showExceptions = true
+        showCauses = true
+    }
+
+    finalizedBy(tasks.jacocoTestReport) // report is always generated after tests run
+}
+tasks.jacocoTestReport {
+    dependsOn(tasks.test) // tests are required to run before generating the report
+    dependsOn(tasks.jacocoTestCoverageVerification)
+}
+
+
+jacoco {
+    toolVersion = "0.8.6"
+    reportsDirectory.set(file("$buildDir/customJacocoReportDir"))
+}
+
+tasks.jacocoTestReport {
+    reports {
+        xml.isEnabled = false
+        csv.isEnabled = false
+        html.destination = file("${buildDir}/jacocoHtml")
+    }
+}
+
+tasks.jacocoTestCoverageVerification {
+    violationRules {
+        rule {
+            limit {
+                minimum = "0.95".toBigDecimal()
+            }
+        }
+    }
+}
+
+configurations.all {
+    resolutionStrategy {
+// Fail eagerly on version conflict (includes transitive dependencies)
+// e.g. multiple different versions of the same dependency (group and name are equal)
+        failOnVersionConflict()
+    }
+}
