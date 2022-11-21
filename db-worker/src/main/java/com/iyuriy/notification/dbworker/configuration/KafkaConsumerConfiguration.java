e1c5140f9727cea9c034d313361eef0a14fef8b4
Overflow922 <genius922@yandex.ru>
2022-11-21 19:44:06 +0300

    Save task payload to db

--- db-worker/src/main/java/com/iyuriy/notification/dbworker/configuration/KafkaConsumerConfiguration.java
+++ db-worker/src/main/java/com/iyuriy/notification/dbworker/configuration/KafkaConsumerConfiguration.java
@@ -1,5 +1,6 @@
 package com.iyuriy.notification.dbworker.configuration;
 
+import com.iyuriy.notification.common.entities.AddNotification;
 import lombok.AllArgsConstructor;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
@@ -9,6 +10,7 @@ import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -20,18 +22,19 @@ public class KafkaConsumerConfiguration {
     private KafkaConsumerProperties properties;
 
     @Bean
-    public ConsumerFactory<String, String> consumerFactory() {
+    public ConsumerFactory<String, AddNotification> consumerFactory() {
         Map<String, Object> props = new HashMap<>();
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, properties.getBootstrapAddress());
         props.put(ConsumerConfig.GROUP_ID_CONFIG, properties.getGroupId());
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        return new DefaultKafkaConsumerFactory<>(props);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+        return new DefaultKafkaConsumerFactory<>(props, new StringDeserializer(),
+                new JsonDeserializer<>(AddNotification.class));
     }
 
     @Bean
-    public ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory() {
-        ConcurrentKafkaListenerContainerFactory<String, String> factory =
+    public ConcurrentKafkaListenerContainerFactory<String, AddNotification> kafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, AddNotification> factory =
                 new ConcurrentKafkaListenerContainerFactory<>();
         factory.setConsumerFactory(consumerFactory());
         return factory;
--- db-worker/src/main/java/com/iyuriy/notification/dbworker/configuration/KafkaConsumerService.java
+++ db-worker/src/main/java/com/iyuriy/notification/dbworker/configuration/KafkaConsumerService.java
@@ -1,16 +1,43 @@
 package com.iyuriy.notification.dbworker.configuration;
 
 import com.iyuriy.notification.common.entities.AddNotification;
+import com.iyuriy.notification.dbworker.model.Schedule;
+import com.iyuriy.notification.dbworker.model.Task;
+import com.iyuriy.notification.dbworker.repository.ScheduleRepository;
+import com.iyuriy.notification.dbworker.repository.TaskRepository;
+import lombok.AllArgsConstructor;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class KafkaConsumerService {
+
+    private final ScheduleRepository scheduleRepository;
+    private final TaskRepository taskRepository;
 
     @KafkaListener(topics = "notification-add", groupId = "foo")
     public void addNotificationEvent(@Payload ConsumerRecord<String, AddNotification> message) {
-        System.out.println("Received Message in group foo: " + message);
+        AddNotification value = message.value();
+        Task task = createTask(value);
+        taskRepository.save(task);
+        Schedule schedule = createSchedule(value, task);
+        scheduleRepository.save(schedule);
+    }
+
+    private Schedule createSchedule(AddNotification value, Task task) {
+        return Schedule.builder()
+                .timestamp(value.getTimeToTrigger())
+                .task(task)
+                .build();
+    }
+
+    private Task createTask(AddNotification value) {
+        return Task.builder()
+                .text(value.getNotificationText())
+                .originalRequest(value.getOriginalRq())
+                .build();
     }
 }
