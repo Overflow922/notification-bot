59ff93f77d63b73be98c93eb8283f7dda6b0409b
temychpp <temychp@gmail.com>
2023-04-11 14:12:31 +0300

    change sql db, add ScheduleEvent, add DbAdapter

---
+++ db-adapter/src/main/java/com/iyuriy/db_adapter/models/DbAdapter.java
@@ -0,0 +1,4 @@
+package com.iyuriy.db_adapter.models;
+
+public class DbAdapter {
+}
---
+++ db-adapter/src/main/java/com/iyuriy/db_adapter/repositories/DbAdapterRepository.java
@@ -0,0 +1,9 @@
+package com.iyuriy.db_adapter.repositories;
+
+import com.iyuriy.db_adapter.models.DbAdapter;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DbAdapterRepository extends JpaRepository<DbAdapter, Integer> {
+
+
+}
