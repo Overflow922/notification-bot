4e9099f6bb1d42730eed2cae1c0f3be753f88cb6
temychpp <temychp@gmail.com>
2023-04-14 16:29:30 +0300

    fix something 3

--- db-worker/src/main/java/com/iyuriy/notification/dbworker/DbWorker.java
+++ db-worker/src/main/java/com/iyuriy/notification/dbworker/DbWorker.java
@@ -2,7 +2,9 @@ package com.iyuriy.notification.dbworker;
 
 import com.iyuriy.notification.dbworker.services.FindNewScheduleServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class DbWorker {
 
     private final FindNewScheduleServiceImpl findNewScheduleServiceImpl;
