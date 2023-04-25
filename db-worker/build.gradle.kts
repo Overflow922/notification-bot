7891dce15be1f1b9f994e2b6b99304a472c13048
Overflow922 <genius922@yandex.ru>
2023-04-25 21:25:24 +0300

    #16: Dependency housekeeping

--- db-worker/build.gradle.kts
+++ db-worker/build.gradle.kts
@@ -11,8 +11,10 @@ repositories {
     mavenCentral()
 }
 
+val versions: Map<String, String> by rootProject.extra
 dependencies {
     implementation(project(":common"))
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("com.fasterxml.jackson.core:jackson-databind")
@@ -20,49 +22,21 @@ dependencies {
 
     runtimeOnly("org.postgresql:postgresql")
 
-    compileOnly("org.projectlombok:lombok:1.18.24")
-    annotationProcessor("org.projectlombok:lombok:1.18.24")
-    testCompileOnly("org.projectlombok:lombok:1.18.24")
-    testAnnotationProcessor("org.projectlombok:lombok:1.18.24")
+    compileOnly("org.projectlombok:lombok:${versions["lombok"]}")
+    annotationProcessor("org.projectlombok:lombok:${versions["lombok"]}")
+    testCompileOnly("org.projectlombok:lombok:${versions["lombok"]}")
+    testAnnotationProcessor("org.projectlombok:lombok:${versions["lombok"]}")
 
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.0")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:${versions["junit"]}")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:${versions["junit"]}")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 
-    implementation("org.hibernate.validator:hibernate-validator:8.0.0.Final")
-    implementation("org.modelmapper:modelmapper:3.1.1")
+    implementation("org.hibernate.validator:hibernate-validator:${versions["hibernate"]}")
+    implementation("org.modelmapper:modelmapper:${versions["model-mapper"]}")
 }
 
 repositories {
     mavenCentral()
-}
-
-dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-web")
-    dependencies {
-        implementation(project(":common"))
-        implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-        implementation("com.fasterxml.jackson.core:jackson-databind")
-        implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
-
-//        implementation("org.liquibase:liquibase-core")
-
-        runtimeOnly("org.postgresql:postgresql")
-
-        compileOnly("org.projectlombok:lombok:1.18.24")
-        annotationProcessor("org.projectlombok:lombok:1.18.24")
-        testCompileOnly("org.projectlombok:lombok:1.18.24")
-        testAnnotationProcessor("org.projectlombok:lombok:1.18.24")
-
-        testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.0")
-        testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.0")
-        testImplementation("org.springframework.boot:spring-boot-starter-test")
-
-        implementation("org.hibernate.validator:hibernate-validator:8.0.0.Final")
-        implementation("org.modelmapper:modelmapper:3.1.1")
-        implementation("org.springframework:spring-web:5.3.26")
-        implementation("org.springframework:spring-jdbc:6.0.6")
-    }
 }
 
 tasks.getByName<Test>("test") {
